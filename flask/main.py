ea8ba284c1b4f0ea60f1a49e1fc569453ec40a3b
srujit12091997 <varasala_va@srmuniv.edu>
2023-12-12 14:42:37 -0500

    chnages in main file

--- flask/main.py
+++ flask/main.py
@@ -2,7 +2,7 @@ import os
 
 from flask import Flask, request
 
-app = Flask(_name_)
+app = Flask(__name__)
 
 
 @app.route('/')
@@ -11,5 +11,5 @@ def hello_world():
     return f'Hello {name}!'
 
 
-if _name_ == "_main_":
+if __name__ == "_main_":
     app.run(debug=True, host='0.0.0.0', port=int(os.environ.get('PORT', 3000)))
